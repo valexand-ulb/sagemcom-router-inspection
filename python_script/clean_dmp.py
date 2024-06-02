293a1963e54698a9901a7d536b47d3b81fbb4184
Vinovrski Alexandre <vinovrski.alexandre@ulb.be>
2024-06-02 12:03:07 +0200

    file removerd and fallback to previous dump

--- python_script/clean_dmp.py
+++ python_script/clean_dmp.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+KEEP_OOB = int(sys.argv[1])
 
 def delete_lines(file_path):
     with open(file_path, 'r') as file:
@@ -6,11 +9,15 @@ def delete_lines(file_path):
     
     # Remove the first two lines, the 131st line, and the last line
     del lines[:2]
-    del lines[128]  # 131st line after removing the first two lines
-    del lines[-1]
 
-    # Remove leading tabulation spaces from each line
-    lines = [line.lstrip('\t').rstrip('\n') for line in lines]
+    if KEEP_OOB:
+        del lines[128]  # 131st line after removing the first two lines
+        del lines[-1]
+    else:
+        del lines[128:]
+
+    # Remove leading tabulation, replace double space by space, replace \n to space
+    lines = [line.lstrip('\t').replace('  ', ' ').replace('\n', ' ') for line in lines]
 
     with open(file_path, 'w') as file:
         file.writelines(lines)
